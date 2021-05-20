1a4b99d5727f3afa24ee90cdf6f0daffb9593902
ElemenTP <fpjraidden@outlook.com>
2021-05-20 11:19:09 +0800

    Update

--- task1/global.hpp
+++ task1/global.hpp
@@ -33,7 +33,8 @@ protected:
 
 public:
     virtual item_type getItemType() const = 0;
-    Merchandise();
+    virtual void optItemDes() const = 0;
+    Merchandise() {}
     Merchandise(string ipttitle, string iptdes, double iptpricing, float iptdiscount, unsigned long long iptstock) : title(ipttitle), description(iptdes), pricing(iptpricing), discount(iptdiscount), stock(iptstock) {}
     string getTitle() const { return title; }
     string getDescription() const { return description; }
@@ -54,8 +55,17 @@ class Food : public Merchandise
 {
 public:
     item_type getItemType() const { return food; }
-    Food();
+    Food() {}
     Food(string ipttitle, string iptdes, double iptpricing, float iptdiscount, unsigned long long iptstock) : Merchandise(ipttitle, iptdes, iptpricing, iptdiscount, iptstock) {}
+    void optItemDes() const
+    {
+        cout << "种类" << setw(50) << "食品" << endl;
+        cout << "名称" << setw(50) << title << endl;
+        cout << "详情" << setw(50) << description << endl;
+        cout << "售价" << setw(50) << pricing << endl;
+        cout << "折扣" << setw(50) << discount << endl;
+        cout << "库存" << setw(50) << stock << endl;
+    }
     friend void to_json(json &j, const Food &p);
     friend void from_json(const json &j, Food &p);
 };
@@ -77,8 +87,17 @@ class Clothing : public Merchandise
 {
 public:
     item_type getItemType() const { return clothing; }
-    Clothing();
+    Clothing() {}
     Clothing(string ipttitle, string iptdes, double iptpricing, float iptdiscount, unsigned long long iptstock) : Merchandise(ipttitle, iptdes, iptpricing, iptdiscount, iptstock) {}
+    void optItemDes() const
+    {
+        cout << "种类" << setw(50) << "衣物" << endl;
+        cout << "名称" << setw(50) << title << endl;
+        cout << "详情" << setw(50) << description << endl;
+        cout << "售价" << setw(50) << pricing << endl;
+        cout << "折扣" << setw(50) << discount << endl;
+        cout << "库存" << setw(50) << stock << endl;
+    }
     friend void to_json(json &j, const Clothing &p);
     friend void from_json(const json &j, Clothing &p);
 };
@@ -103,8 +122,17 @@ protected:
 
 public:
     item_type getItemType() const { return type; }
-    Book();
+    Book() {}
     Book(string ipttitle, string iptdes, double iptpricing, float iptdiscount, unsigned long long iptstock) : Merchandise(ipttitle, iptdes, iptpricing, iptdiscount, iptstock) {}
+    void optItemDes() const
+    {
+        cout << "种类" << setw(50) << "书籍" << endl;
+        cout << "名称" << setw(50) << title << endl;
+        cout << "详情" << setw(50) << description << endl;
+        cout << "售价" << setw(50) << pricing << endl;
+        cout << "折扣" << setw(50) << discount << endl;
+        cout << "库存" << setw(50) << stock << endl;
+    }
     friend void to_json(json &j, const Book &p);
     friend void from_json(const json &j, Book &p);
 };
@@ -131,7 +159,7 @@ protected:
 
 public:
     virtual usr_type getUserType() const = 0;
-    Account(){}
+    Account() {}
     Account(string iptname, string iptpasswd) : name(iptname), passwd(iptpasswd), balance(0) {}
     bool changeName(string iptpasswd, string newname)
     {
@@ -189,7 +217,7 @@ class Client : public Account
 {
 public:
     usr_type getUserType() const { return client; }
-    Client(){}
+    Client() {}
     Client(string iptname, string iptpasswd) : Account(iptname, iptpasswd)
     {
         cout << "成功创建新消费者账号，用户名：" << name << endl;
@@ -216,7 +244,7 @@ protected:
 
 public:
     usr_type getUserType() const { return business; }
-    Business(){}
+    Business() {}
     Business(string iptname, string iptpasswd) : Account(iptname, iptpasswd)
     {
         cout << "成功创建新商家账号，用户名：" << name << endl;
--- task1/main.cpp
+++ task1/main.cpp
@@ -4,126 +4,283 @@ using json = nlohmann::json;
 
 int main(void)
 {
+    system("chcp 65001");
     json data;
-    Account *cur_usr;
-    vector<const json *> allShelves;
+    Account *cur_usr = NULL;
+    int usr_ptr;
     ifstream iptdata("datafile.json");
-    ofstream optdata("datafile.json");
+    ofstream optdata;
     if (iptdata)
         iptdata >> data;
-START:
-    system("cls");
-    cout << "**********************电商交易平台**********************" << endl;
-    try
+    iptdata.close();
+    bool start_page_state = true, content_page_state;
+    while (true)
     {
-        cout << setw(20) << "输入1" << setw(40) << "注册新账户" << endl;
-        if (!data.empty())
-        {
-            cout << setw(20) << "输入2" << setw(40) << "登录已有账户" << endl;
-            cout << setw(20) << "输入3" << setw(40) << "以游客身份浏览货架" << endl;
-        }
-        int ipt;
-        cin >> ipt;
         system("cls");
-        switch (ipt)
+        while (start_page_state)
         {
-        case 1:
-            cout << setw(20) << "输入1" << setw(40) << "注册新客户账户" << endl;
-            cout << setw(20) << "输入2" << setw(40) << "注册新商家账户" << endl;
-            int ipt;
-            cin >> ipt;
-            system("cls");
-            switch (ipt)
+            cout << "**********************电商交易平台**********************" << endl;
+            try
             {
-            case 1:
-            {
-                string iptname, iptpasswd;
-                cout << setw(20) << "输入" << setw(40) << "新账户的名称" << endl;
-                cin >> iptname;
-                cout << setw(20) << "输入" << setw(40) << "新账户的密码" << endl;
-                cin >> iptpasswd;
-                if (iptpasswd.empty())
+                cout << "输入1" << setw(50) << "注册新账户" << endl;
+                if (!data.empty())
                 {
-                    throw "错误：密码不能为空\n";
+                    cout << "输入2" << setw(50) << "登录已有账户" << endl;
+                    cout << "输入3" << setw(50) << "以游客身份浏览货架" << endl;
+                }
+                int ipt;
+                cin >> ipt;
+                system("cls");
+                switch (ipt)
+                {
+                case 1:
+                    cout << "输入1" << setw(50) << "注册新客户账户" << endl;
+                    cout << "输入2" << setw(50) << "注册新商家账户" << endl;
+                    cout << "输入3" << setw(50) << "返回" << endl;
+                    int ipt;
+                    cin >> ipt;
+                    system("cls");
+                    switch (ipt)
+                    {
+                    case 1:
+                    {
+                        string iptname, iptpasswd;
+                        cout << "输入" << setw(50) << "新账户的名称" << endl;
+                        cin >> iptname;
+                        for (int it = 0; it < data.size(); it++)
+                        {
+                            if (data[it]["name"].get<string>() == iptname)
+                                throw "错误：该名称已被使用\n";
+                        }
+                        cout << "输入" << setw(50) << "新账户的密码" << endl;
+                        cin >> iptpasswd;
+                        cur_usr = new Client(iptname, iptpasswd);
+                        json j = *(Client *)cur_usr;
+                        usr_ptr = data.size();
+                        data.push_back(j);
+                        optdata.open("datafile.json");
+                        optdata << setw(4) << data;
+                        optdata.close();
+                        start_page_state = false;
+                        break;
+                    }
+                    case 2:
+                    {
+                        string iptname, iptpasswd;
+                        cout << "输入" << setw(50) << "新账户的名称" << endl;
+                        cin >> iptname;
+                        for (int it = 0; it < data.size(); it++)
+                        {
+                            if (data[it]["name"].get<string>() == iptname)
+                                throw "错误：该名称已被使用\n";
+                        }
+                        cout << "输入" << setw(50) << "新账户的密码" << endl;
+                        cin >> iptpasswd;
+                        cur_usr = new Business(iptname, iptpasswd);
+                        json j = *(Business *)cur_usr;
+                        usr_ptr = data.size();
+                        data.push_back(j);
+                        optdata.open("datafile.json");
+                        optdata << setw(4) << data;
+                        optdata.close();
+                        start_page_state = false;
+                        break;
+                    }
+                    case 3:
+                        break;
+
+                    default:
+                        throw "错误:输入不合法\n";
+                    }
+                    break;
+
+                case 2:
+                {
+                    if (data.empty())
+                        throw "错误:输入不合法\n";
+                    string iptname, iptpasswd;
+                    cout << "输入" << setw(50) << "账户的名称" << endl;
+                    cin >> iptname;
+                    cout << "输入" << setw(50) << "账户的密码" << endl;
+                    cin >> iptpasswd;
+                    for (int it = 0; it < data.size(); it++)
+                    {
+                        if (data[it]["name"].get<string>() == iptname && data[it]["passwd"].get<string>() == iptpasswd)
+                        {
+                            usr_ptr = it;
+                            if (data[it]["type"].get<int>() == client)
+                            {
+                                cur_usr = new Client();
+                                *(Client *)cur_usr = data[it];
+                            }
+                            else if (data[it]["type"].get<int>() == business)
+                            {
+                                cur_usr = new Business();
+                                *(Business *)cur_usr = data[it];
+                            }
+                        }
+                    }
+                    if (!cur_usr)
+                        throw "错误：用户名或密码错误\n";
+                    else
+                        start_page_state = false;
                     break;
                 }
-                cur_usr = new Client(iptname, iptpasswd);
-                json j = *(Client *)cur_usr;
-                data.push_back(j);
-                optdata << setw(4) << data;
-                break;
-            }
-            case 2:
-            {
-                string iptname, iptpasswd;
-                cout << setw(20) << "输入" << setw(40) << "新账户的名称" << endl;
-                cin >> iptname;
-                cout << setw(20) << "输入" << setw(40) << "新账户的密码" << endl;
-                cin >> iptpasswd;
-                if (iptpasswd.empty())
-                {
-                    throw "错误：密码不能为空\n";
-                    break;
+
+                case 3:
+                    if (!data.empty())
+                    {
+                        start_page_state = false;
+                        break;
+                    }
+
+                default:
+                    throw "错误:输入不合法\n";
                 }
-                cur_usr = new Business(iptname, iptpasswd);
-                json j = *(Business *)cur_usr;
-                data.push_back(j);
-                optdata << setw(4) << data;
-                break;
             }
-
-            default:
-                throw "错误:输入不合法\n";
+            catch (const char *msg)
+            {
+                cout << msg << endl;
+                system("pause");
             }
-            break;
-
-        case 2:
+        }
+        content_page_state = true;
+        system("cls");
+        while (content_page_state)
         {
-            if (data.empty())
-                break;
-            string iptname, iptpasswd;
-            cout << setw(20) << "输入" << setw(40) << "账户的名称" << endl;
-            cin >> iptname;
-            cout << setw(20) << "输入" << setw(40) << "账户的密码" << endl;
-            cin >> iptpasswd;
-            if (iptpasswd.empty())
+            switch (cur_usr == NULL)
             {
-                throw "错误：密码不能为空\n";
+            case false:
+            {
                 break;
             }
-            for (int it = 0; it < data.size(); it++)
+            case true:
             {
-                if (data[it]["name"].get<string>() == iptname && data[it]["passwd"].get<string>() == iptpasswd)
+                int acc_ptr = 0;
+                while (data[acc_ptr]["type"].get<int>() != business)
                 {
-                    if (data[it]["type"].get<int>() == client)
+                    acc_ptr++;
+                    if (acc_ptr == data.size())
+                        break;
+                }
+                if (acc_ptr < data.size())
+                {
+                    int ipt = 0;
+                    do
                     {
-                        cur_usr = new Client();
-                        *(Client *)cur_usr = data[it];
-                    }
-                    else if (data[it]["type"].get<int>() == business)
+                        Business acc_tmp = data[acc_ptr];
+                        for (int mer_ptr = 0; mer_ptr < (*(acc_tmp.getMyShelves())).size() && mer_ptr > -1 && content_page_state;)
+                        {
+                            bool input_bad;
+                            do
+                            {
+                                cout << "**********************全部商品**********************" << endl;
+                                switch ((*(acc_tmp.getMyShelves()))[mer_ptr]["type"].get<int>())
+                                {
+                                case food:
+                                {
+                                    Food mer_tmp = (*(acc_tmp.getMyShelves()))[mer_ptr];
+                                    mer_tmp.optItemDes();
+                                    break;
+                                }
+                                case clothing:
+                                {
+                                    Clothing mer_tmp = (*(acc_tmp.getMyShelves()))[mer_ptr];
+                                    mer_tmp.optItemDes();
+                                    break;
+                                }
+                                case book:
+                                {
+                                    Book mer_tmp = (*(acc_tmp.getMyShelves()))[mer_ptr];
+                                    mer_tmp.optItemDes();
+                                    break;
+                                }
+                                }
+                                cout << "输入1" << setw(50) << "下一个" << endl;
+                                cout << "输入2" << setw(50) << "上一个" << endl;
+                                cout << "输入3" << setw(50) << "退出" << endl;
+                                cin >> ipt;
+                                switch (ipt)
+                                {
+                                case 1:
+                                    mer_ptr++;
+                                    input_bad = false;
+                                    break;
+
+                                case 2:
+                                    mer_ptr--;
+                                    input_bad = false;
+                                    break;
+
+                                case 3:
+                                    content_page_state = false;
+                                    input_bad = false;
+                                    break;
+                                default:
+                                    system("cls");
+                                    cout << "错误：输入不合法";
+                                    system("pause");
+                                    input_bad = true;
+                                    system("cls");
+                                }
+                            } while (input_bad);
+                            system("cls");
+                        }
+                        int acc_ptr_bkp = acc_ptr;
+                        switch (ipt)
+                        {
+                        case 1:
+                        {
+                            while (data[acc_ptr]["type"].get<int>() != business)
+                            {
+                                acc_ptr++;
+                                if (acc_ptr == data.size())
+                                    break;
+                            }
+                            if (acc_ptr == data.size())
+                            {
+                                cout << "没有更多了！\n";
+                                system("pause");
+                                acc_ptr = acc_ptr_bkp;
+                                system("cls");
+                            }
+                        }
+                        case 2:
+                        {
+                            while (data[acc_ptr]["type"].get<int>() != business)
+                            {
+                                acc_ptr--;
+                                if (acc_ptr == -1)
+                                    break;
+                            }
+                            if (acc_ptr < 0)
+                            {
+                                cout << "没有更多了！\n";
+                                system("pause");
+                                acc_ptr = acc_ptr_bkp;
+                                system("cls");
+                            }
+                        }
+                        }
+                    } while (content_page_state);
+                    if (ipt == 0)
                     {
-                        cur_usr = new Business();
-                        *(Business *)cur_usr = data[it];
+                        cout << "没有商品可以展示。\n";
+                        system("pause");
+                        system("cls");
                     }
                 }
+                else
+                {
+                    cout << "没有商品可以展示。\n";
+                    system("pause");
+                    system("cls");
+                }
+                if (cur_usr)
+                    content_page_state = true;
             }
-            if (!cur_usr)
-                throw "错误：用户名或密码错误\n";
-            break;
+            }
         }
-
-        case 3:
-            if (!data.empty())
-                break;
-
-        default:
-            throw "错误:输入不合法\n";
-            break;
-        }
-    }
-    catch (const char *msg)
-    {
-        cout << *msg << endl;
-        goto START;
+        start_page_state = true;
     }
 }
